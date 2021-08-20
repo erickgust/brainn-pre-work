2c306189dc398fb6e1c866044ffa69b1da89b3d7
Erick Gustavo <erick.stazs@gmail.com>
2021-08-20 12:48:02 -0300

    Create the color selection element

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -14,3 +14,50 @@ inputName.addEventListener('input', (event) => {
     : toCapitalize(word)
   }).join(' ')
 })
+
+const form = document.querySelector('[data-js="form"]')
+const select = document.createElement('select')
+const colors = [
+  {roxo: "#4A1942"},
+  {azul: "#228CDB"},
+  {verde: "#2CDA9D"},
+  {amarelo: "#FCFC62"},
+  {laranja: "#EB5E28"},
+]
+const divContainer = document.createElement('div')
+divContainer.style.display = 'flex'
+divContainer.style.flexWrap = 'wrap'
+
+function createOption(color) {
+  const option = document.createElement('option')
+  option.value = Object.values(color)
+  option.textContent = toCapitalize(Object.keys(color)[0])
+  return option
+}
+
+function createDiv(value) {
+  const div = document.createElement('div')
+  div.style.width = '100px'
+  div.style.height = '100px'
+  div.style.backgroundColor = value
+
+  return div
+}
+
+colors.forEach(color => {
+  const newOption = createOption(color)
+  select.appendChild(newOption)
+})
+
+select.addEventListener('change', (event) => {
+  divContainer.innerHTML = ''
+
+  Array.from(event.target.selectedOptions).forEach(input => {
+    const newDiv = createDiv(input.value)
+    divContainer.appendChild(newDiv)
+  })
+})
+
+select.setAttribute('multiple', '')
+form.appendChild(select)
+document.body.appendChild(divContainer)
